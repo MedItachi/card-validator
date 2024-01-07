6beaef38c61eb8b5c6e3e2f73c281766567ce5c6
Mohamed El Fadil <medelfadil193@gmail.com>
2024-01-07 17:37:07 +0100

    brcxc

--- src/main/java/org/java/mql/services/CardService.java
+++ src/main/java/org/java/mql/services/CardService.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.java.mql.models.Card;
 
 public interface CardService {
-	
+
 	public boolean isCardNumberExists(String cardNumber);
+
 	public void saveCard(Card card);
+
 	public List<Card> getAll();
 
 }
