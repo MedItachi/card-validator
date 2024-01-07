f87f680e4ee13367b110edcf8a1b87c5de8b65e0
Mohamed El Fadil <medelfadil193@gmail.com>
2024-01-07 18:10:11 +0100

    remove test Scanner

--- src/main/java/org/java/mql/WebService1Application.java
+++ src/main/java/org/java/mql/WebService1Application.java
@@ -2,8 +2,10 @@ package org.java.mql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "org.java.mql" })
 public class WebService1Application {
 
 	public static void main(String[] args) {
